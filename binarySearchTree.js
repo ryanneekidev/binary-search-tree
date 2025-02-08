fe18dacc4085ad6d47060f6f8c1b04ecbf4c8029
Rayan Mamhoud <kiraclover@protonmail.com>
2025-02-08 20:48:53 +0100

    Add insert function and print function

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -30,3 +30,29 @@ function createTree(array, start, end){
     rootNode.setRight(createTree(array, middle+1, end));
     return rootNode;
 }
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+function insertNode(tree, node){
+    if (!tree) {
+        return node;
+    }
+
+        if (node.data < tree.data) {
+            tree.leftNode = insertNode(tree.leftNode, node);
+        } else {
+            tree.rightNode = insertNode(tree.rightNode, node);
+        }
+        return tree;
+}
